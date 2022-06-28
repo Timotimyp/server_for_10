b37124e6b669165ce00c17460f5795854f4eb428
Timotimyp <60345296+Timotimyp@users.noreply.github.com>
2022-06-28 06:55:06 +0300

    Update news_api.py

--- data/news_api.py
+++ data/news_api.py
@@ -296,20 +296,20 @@ def get_info(category, position):
 @blueprint.route('/api/get_porog', methods=['GET'])
 def get_porog():
     db_sess = db_session.create_session()
-    fi = db_sess.query(cat).filter(delivery.delivery == "Порог").first()
+    fi = db_sess.query(delivery).filter(delivery.delivery == "Порог").first()
     return jsonify({'porog': fi.cost})
 
 
 @blueprint.route('/api/get_samdelivery', methods=['GET'])
 def get_samdelivery():
     db_sess = db_session.create_session()
-    fi = db_sess.query(cat).filter(delivery.delivery == "Самовывоз").first()
+    fi = db_sess.query(delivery).filter(delivery.delivery == "Самовывоз").first()
     return jsonify({'samdelivery': fi.cost})
 
 
 @blueprint.route('/api/get_delivery', methods=['GET'])
 def get_delivery():
     db_sess = db_session.create_session()
-    fi = db_sess.query(cat).filter(delivery.delivery == "Доставка").first()
+    fi = db_sess.query(delivery).filter(delivery.delivery == "Доставка").first()
     return jsonify({'delivery': fi.cost})
 
